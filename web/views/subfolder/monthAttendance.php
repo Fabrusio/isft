0f8276663516c28cdb98d9e78fae0c2bad3314e1
Fabrusio <fabrusio3p3@gmail.com>
2024-11-27 23:58:11 -0300

    Agregados dos modales para los botones de inserción y edición

--- web/views/subfolder/monthAttendance.php
+++ web/views/subfolder/monthAttendance.php
@@ -43,3 +43,66 @@
         </div>
     </div>
 <?php endif; ?>
+
+<?php foreach ($dataMonths as $months) : ?>
+    <div class="modal fade cierreModal" id="assignAttendanceModal<?php echo $months['id_month']; ?>" tabindex="-1" role="dialog" aria-labelledby="editUserModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header alert alert-success">
+                    <h5 class="modal-title" id="editUserModalLabel"><strong>Ingresar asistencia</strong></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form id="insertstudentattendance">
+                        <?php
+                        $dataAttendance = TeacherController::getAllStudentDataAttendance($_GET['id_subject'], $months['id_month'], $_GET['id_student']);
+                        ?>
+                        <input type="hidden" name="id_subject" value="<?php echo $_GET['id_subject']; ?>">
+                        <input type="hidden" name="id_month" value="<?php echo $months['id_month']; ?>">
+                        <input type="hidden" name="id_student" value="<?php echo $_GET['id_student']; ?>">
+
+                        <div class="form-group">
+                            <label for="schedule">Cantidad total de horas asistidas en el mes</label>
+                            <input type="number" min="0" class="form-control" id="attendance" name="attendance" value="<?php echo $dataAttendance['monthly_attendance']; ?>">
+                        </div>
+                        <button type="submit" class="btn btn-success ladda-button">Guardar cambios</button>
+                        <div class="response-message text-center"></div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade cierreModal" id="editAttendanceModal<?php echo $months['id_month']; ?>" tabindex="-1" role="dialog" aria-labelledby="editUserModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header alert alert-primary">
+                    <h5 class="modal-title" id="editUserModalLabel"><strong>Editar asistencia</strong></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form id="editstudentattendance">
+                        <?php
+                        $dataAttendance = TeacherController::getAllStudentDataAttendance($_GET['id_subject'], $months['id_month'], $_GET['id_student']);
+                        ?>
+                        <input type="hidden" name="id_student_attendance" value="<?php echo $dataAttendance['id_student_attendance']; ?>">
+                        <input type="hidden" name="id_subject" value="<?php echo $_GET['id_subject']; ?>">
+                        <input type="hidden" name="id_month" value="<?php echo $months['id_month']; ?>">
+
+                        <div class="form-group">
+                            <label for="schedule">Cantidad total de horas asistidas en el mes</label>
+                            <input type="number" min="0" class="form-control" id="attendance" name="attendance" value="<?php echo $dataAttendance['monthly_attendance']; ?>">
+                        </div>
+                        <button type="submit" class="btn btn-primary ladda-button">Guardar cambios</button>
+                        <div class="response-message text-center"></div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php endforeach; ?>
